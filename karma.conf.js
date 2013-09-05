5b03f57e956c657c39706516e20f8becbbf8dc2a
toran billups <toranb@gmail.com>
2013-09-04 19:57:53 -0500

    upgraded to ember.js 1.0

--- karma.conf.js
+++ karma.conf.js
@@ -4,8 +4,8 @@ module.exports = function(karma) {
 
         files: [
           "vendor/jquery/jquery.min.js",
-          "handlebars.js",
-          "ember.js",
+          "vendor/handlebars/handlebars.js",
+          "vendor/ember/ember.js",
           "vendor/jquery-mockjax/jquery.mockjax.js",
           "app.js",
           "tests/*.js",
